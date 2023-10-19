de6d860707620c503661ef48cb896c1072274a95
Darina Usanova <darinushkaaaa@gmail.com>
2023-10-19 17:49:31 +0200

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const newMap = new Map(bracketsConfig);
+
+  for (let char of str) {
+    if (newMap.has(char)){
+      if (newMap.get(char) !== char){
+        stack.push(char);
+      } else {
+        if (stack.length === 0 || stack[stack.length - 1] !== char){
+          stack.push(char)
+        } else{
+          stack.pop()
+        }
+      }
+    } else{
+      const lastChar = stack.pop();
+      if (newMap.get(lastChar) !== char){
+        return false;
+      }
+    }
+  }
+  return !stack.length
+};
+
